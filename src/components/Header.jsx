ce186c0ebf7beacc8b88efb45418e934e3aa22e2
Marco Bacci <marcobacci94@gmail.com>
2025-08-25 16:01:19 +0200

    feat: header structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="header-col">
+          <div id="logo">
+            <img src="/img/dc-logo.png" alt="logo" />
+          </div>
+        </div>
+        <div className="header-col">
+          <ul id='menu'>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
