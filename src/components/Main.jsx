ebfe1d1c3bbc2179d53eab9f9a08818a4805e1c7
Marco Bacci <marcobacci94@gmail.com>
2025-08-25 16:35:14 +0200

    feat: add Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2>-- Content goes here -- </h2>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
