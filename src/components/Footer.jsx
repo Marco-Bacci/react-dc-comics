46c9819aee5806550ec8bb0d9dfb738193f290ae
Marco Bacci <marcobacci94@gmail.com>
2025-08-25 17:06:06 +0200

    feat: add footer first list with imgs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        
+      <div className="footer-list bg-blue">
+        <div className="container">
+          <ul>
+            <li>
+              <img src="/img/buy-comics-digital-comics.png" alt="" />
+              <a href="#">DIGITAL COMICS</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src="public/img/buy-comics-merchandise.png" alt="" />
+              <a href="#">DC MERCHANDISE</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src="public/img/buy-comics-subscriptions.png" alt="" />
+              <a href="#">SUBSCRIPTION</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src="public/img/buy-comics-shop-locator.png" alt="" />
+              <a href="#">COMIC SHOP LOCATOR</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src="public/img/buy-dc-power-visa.svg" alt="" />
+              <a href="#">DC POWER VISA</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
