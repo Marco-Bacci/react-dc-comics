4a61b8447c87c44e52d521e09998db5b482716a3
Marco Bacci <marcobacci94@gmail.com>
2025-08-26 16:27:35 +0200

    feat: crerate navbar component and import into Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./Navbar";
 const Header = () => {
   return (
     <header>
@@ -9,38 +10,7 @@ const Header = () => {
             </div>
           </div>
           <div className="col-50 d-flex align-items-center">
-            <ul id="menu">
-              <li>
-                <a href="#">CHARACTERS</a>
-              </li>
-              <li>
-                <a href="#">COMICS</a>
-              </li>
-              <li>
-                <a href="#">MOVIES</a>
-              </li>
-              <li>
-                <a href="#">TV</a>
-              </li>
-              <li>
-                <a href="#">GAMES</a>
-              </li>
-              <li>
-                <a href="#">COLLECTIBLES</a>
-              </li>
-              <li>
-                <a href="#">VIDEOS</a>
-              </li>
-              <li>
-                <a href="#">FANS</a>
-              </li>
-              <li>
-                <a href="#">NEWS</a>
-              </li>
-              <li>
-                <a href="#">SHOP</a>
-              </li>
-            </ul>
+            <Navbar />
           </div>
         </div>
       </div>
